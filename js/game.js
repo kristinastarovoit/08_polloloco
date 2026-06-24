4d104d139a1f5e35f14b6b94799e2827c4f5b8aa
kristina <kristina.starovoit@gmail.com>
2026-06-24 15:38:37 +0200

    add: drawImage for classes, clearRect for canvas

--- js/game.js
+++ js/game.js
@@ -1,11 +1,10 @@
 let canvas;
-let ctx;
-
-let world = new World();
+let world;
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    world = new World(canvas); // übergibt canvas in constructor von Worlds
 
     console.log('my character is', world.character);
 }
