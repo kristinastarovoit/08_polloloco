f9e60c6a964ea5eeb9df4cab564cd3a4b52ce9b5
kristina <kristina.starovoit@gmail.com>
2026-06-25 17:09:18 +0200

    refactor: positioning and size for chicken and character

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MoveableObject {
-    // width = 100;
-    // height = 196;
+    width = 100;
+    height = 196;
+    y = 230;
     constructor() {
         super().loadImg('img/2_character_pepe/2_walk/W-21.png');
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,7 @@
 class Chicken extends MoveableObject {
-    y = 290;
+    y = 360;
+    height = 60;
+    width = 60;
     constructor() {
         super().loadImg('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 200 + Math.random() * 500;
