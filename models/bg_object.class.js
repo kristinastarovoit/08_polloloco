d806c7c65658e3c63e0b0cc39ae0ccd2070d29d0
kristina <kristina.starovoit@gmail.com>
2026-06-25 12:55:01 +0200

    add: class BackgroundObject with dimensions

--- models/bg_object.class.js
+++ models/bg_object.class.js
@@ -1,5 +1,9 @@
 class BackgroundObject extends MoveableObject {
-    constructor(imagePath) {
+    width = 720;
+    height = 400;
+    constructor(imagePath, x, y) {
         super().loadImg(imagePath);
+        this.y = y;
+        this.x = x;
     }
 }
