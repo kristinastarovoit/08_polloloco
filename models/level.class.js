3704ba4e4fb4ffa0d5a09aac5f37513305a144c1
kristina <kristina.starovoit@gmail.com>
2026-06-26 15:59:47 +0200

    add: map limitation

--- models/level.class.js
+++ models/level.class.js
@@ -2,6 +2,8 @@ class Level {
     enemies;
     clouds;
     backgroundObjects;
+    level_end_x = 2100;
+
     constructor(enemies, clouds, backgroundObjects) {
         this.enemies = enemies;
         this.clouds = clouds;
