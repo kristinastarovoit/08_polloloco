8f57cf83e6b980c4ca097de1aa3fdaa5c7bc710e
kristina <kristina.starovoit@gmail.com>
2026-06-26 15:31:03 +0200

    add: camera scroll movement

--- models/world.class.js
+++ models/world.class.js
@@ -9,18 +9,32 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', -720),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -720),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -720),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -720),
         new BackgroundObject('img/5_background/layers/air.png', 0),
-
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        // new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 0),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        // new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
-        // new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 0)
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/air.png', 720),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720),
+        new BackgroundObject('img/5_background/layers/air.png', 2 * 720),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 2 * 720),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 2 * 720),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 2 * 720),
+        new BackgroundObject('img/5_background/layers/air.png', 3*720),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 3*720),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 3*720),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 3*720),
     ];
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
+
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
@@ -33,10 +47,12 @@ class World {
     }
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
+        this.ctx.translate(-this.camera_x, 0);
 
         let self = this;
         requestAnimationFrame(function () {
