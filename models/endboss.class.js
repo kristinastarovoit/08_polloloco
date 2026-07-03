e1bed70c2092269c0568acdd4c6f77d2255ef91a
kristina <kristina.starovoit@gmail.com>
2026-07-03 14:11:32 +0200

    add: hurt animation to endboss

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -12,6 +12,31 @@ class Endboss extends MoveableObject {
         'img/4_enemie_boss_chicken/2_alert/G11.png',
         'img/4_enemie_boss_chicken/2_alert/G12.png'
     ];
+    IMAGES_ALERT = [
+        'img/4_enemie_boss_chicken/2_alert/G5.png',
+        'img/4_enemie_boss_chicken/2_alert/G6.png',
+        'img/4_enemie_boss_chicken/2_alert/G7.png',
+        'img/4_enemie_boss_chicken/2_alert/G8.png',
+        'img/4_enemie_boss_chicken/2_alert/G9.png',
+        'img/4_enemie_boss_chicken/2_alert/G10.png',
+        'img/4_enemie_boss_chicken/2_alert/G11.png',
+        'img/4_enemie_boss_chicken/2_alert/G12.png'
+    ];
+    IMAGES_ATTACK = [
+        'img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img/4_enemie_boss_chicken/3_attack/G15.png',
+        'img/4_enemie_boss_chicken/3_attack/G16.png',
+        'img/4_enemie_boss_chicken/3_attack/G17.png',
+        'img/4_enemie_boss_chicken/3_attack/G18.png',
+        'img/4_enemie_boss_chicken/3_attack/G19.png',
+        'img/4_enemie_boss_chicken/3_attack/G20.png'
+    ];
+    IMAGES_HURT = [
+        'img/4_enemie_boss_chicken/4_hurt/G21.png',
+        'img/4_enemie_boss_chicken/4_hurt/G22.png',
+        'img/4_enemie_boss_chicken/4_hurt/G23.png'
+    ];
     IMAGES_DEAD = [
         'img/4_enemie_boss_chicken/5_dead/G24.png',
         'img/4_enemie_boss_chicken/5_dead/G25.png',
@@ -29,6 +54,9 @@ class Endboss extends MoveableObject {
         super();
         this.loadImg(this.IMAGES_WALKING[0]); //wieso hier mit super und drunter mit this?
         this.loadImgs(this.IMAGES_WALKING);
+        this.loadImgs(this.IMAGES_ALERT);
+        this.loadImgs(this.IMAGES_ATTACK);
+        this.loadImgs(this.IMAGES_HURT);
         this.loadImgs(this.IMAGES_DEAD);
         this.x = 2000;
         this.animate();
@@ -39,6 +67,9 @@ class Endboss extends MoveableObject {
             this.playAnimation(this.IMAGES_WALKING);
             if (this.isDead()) {
                 this.playAnimation(this.IMAGES_DEAD);
+            }
+            else if (this.isHurt()) {
+                this.playAnimation(this.IMAGES_HURT);
             }
         }, 1000 / 10);
     }
