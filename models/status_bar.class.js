9670d024d14cadb49aa56f3a36b1b0d3aa6e5e92
kristina <kristina.starovoit@gmail.com>
2026-07-09 11:54:31 +0200

    fix: correct endboss health bar for low energy values

--- models/status_bar.class.js
+++ models/status_bar.class.js
@@ -63,6 +63,8 @@ class StatusBar extends DrawableObject {
             return 2;
         } else if (this.percentage >= 20) {
             return 1;
+        } else if (this.percentage > 0) {
+            return 1;
         } else {
             return 0;
         }
